ebaaa0de07e154d1c6c3732d32648ae6a24df06d
Pininita <pininita92001@gmail.com>
2024-06-23 20:30:30 -0500

    poniendo ruta de imagen correcta, no se me fue posible usar la ruta desde el local

--- homeworks/homework-2/rickandmorty/src/App.jsx
+++ homeworks/homework-2/rickandmorty/src/App.jsx
@@ -13,13 +13,13 @@ function App() {
     },
     {id: 2,
       title: "morthy",
-      url: "https://koradiproductions.com/wp-content/uploads/2022/03/rick-sanchez.jpg",
+      url: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/02fdc37b-d6df-48ac-b5d6-26a71e5c72b0/d9ieocf-5a1296a0-2226-4bb1-9e98-3341e9983350.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAyZmRjMzdiLWQ2ZGYtNDhhYy1iNWQ2LTI2YTcxZTVjNzJiMFwvZDlpZW9jZi01YTEyOTZhMC0yMjI2LTRiYjEtOWU5OC0zMzQxZTk5ODMzNTAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.w6jbRj4WVpQcHM4W4p7-HORqRT0PEuepc1zAMHSEblY",
       genre: "male",
       status: "active"
     },
     {id: 3,
       title: "Summer",
-      url: "https://koradiproductions.com/wp-content/uploads/2022/03/rick-sanchez.jpg",
+      url: "https://comicvine.gamespot.com/a/uploads/original/6/66303/4472085-summer_in_ricksy_business.jpg",
       genre: "female",
       status: "active"
     }
