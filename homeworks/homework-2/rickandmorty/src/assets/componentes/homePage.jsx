a30e092d9b40c14ed397519eb69547d9c49b9f09
Pininita <pininita92001@gmail.com>
2024-07-05 18:30:07 -0500

    adelanto de estilos homepage

--- homeworks/homework-2/rickandmorty/src/assets/componentes/homePage.jsx
+++ homeworks/homework-2/rickandmorty/src/assets/componentes/homePage.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import charactersphoto from './charactersphoto.png'
+import portalgun from './portalgun.png'
 
 // TODO: agrega estilos un diseño personalizado a la página
-function HomePage(props) {
+function HomePage() {
   return (
-    <div>
-      <h1> Bienvenido a la pagina de rick and morty
-      </h1>
+    <div className='home-page'>
+      <div className='home-page-style'>
+        <h1>
+          Welcome to rick and morty web-page
+        </h1>
 
-      <h3> Enlaces rápidos </h3>
-      
-    <div className='enlaces'>
-      <div>
-        <img src="" alt="characters" />
-        <Link to="characters">
-        <button>characters</button>
-        </Link>
-      </div>
-      <div>
-        <img src="" alt="episodes" />
-        <Link to="/episodes">
-        <button>episodes</button>
-        </Link>
+        <h3> Enlaces rápidos </h3>
+
+        <div className='enlaces'>
+          <div className='enlaces-contenido1'>
+            <img className='enlaces-image' src={charactersphoto} alt="characters" />
+            <Link to="characters">
+              <button>characters</button>
+            </Link>
+          </div>
+          <div className='enlaces-contenido2'>
+            <img className='enlaces-image' src={portalgun} alt="episodes" />
+            <Link to="/episodes">
+              <button>episodes</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
 
-
-    </div>
-   
   );
 }
 export default HomePage;
